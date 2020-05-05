6e3e2f825f311b29211463a3ca3cd88935d7c567
Thomas Ederer <thomas@neusiedl.coderdojo.net>
2020-05-05 18:51:38 +0200

    created form for chat app and added shared topic for sending messages

--- src/SharedTopics.js
+++ src/SharedTopics.js
@@ -16,6 +16,7 @@ webapp.shared.topics.COUNTER = '/shared/counter';
 
 
 //                COMMANDS
+
 /**
  * Triggers the server to increment to counter.
  *
@@ -23,3 +24,15 @@ webapp.shared.topics.COUNTER = '/shared/counter';
  *
  */
 webapp.shared.topics.INCREMENT_COUNTER = '/shared/counter/increment';
+
+/**
+ * Informs the server that a client wants to send a message to the other clients.
+ *
+ * The data is an object containing the values (keys):
+ *
+ * - name      : string    the name of the client
+ * - message   : string    the message the client wants to send
+ *
+ * example data: { 'name': 'Thomas', 'message': 'hello world!'}
+ */
+webapp.shared.topics.CHAT_MESSAGE = '/shared/chatmessage';
