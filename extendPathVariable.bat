57fc56a503a9990b91ad77d3045c7333a348d680
Thomas Ederer <thomas@neusiedl.coderdojo.net>
2020-05-07 19:57:41 +0200

    added GRUNT_HOME to PATH

--- extendPathVariable.bat
+++ extendPathVariable.bat
@@ -6,6 +6,8 @@ rem    The following two variables need to be set if the installation folders ar
 set NODE_HOME=D:\software\Node.js\node-v12.16.2-win-x64
 set GIT_HOME=D:\software\Git\PortableGit\cmd
 
+set GRUNT_HOME=%PROJECT_ROOT%\node_modules\grunt\node_modules\.bin
+
 node --version > nul 2>&1
 
 if errorlevel 1 (
@@ -32,6 +34,13 @@ if errorlevel 1 (
 	) else (
 		set "PATH=%PATH%;%GIT_HOME%"
 	)
+)
+
+if not exist %GRUNT_HOME% (
+   echo Home folder "%GRUNT_HOME%" of grunt does not exist! Did you forget to run 'npm install'?
+   pause
+) else (
+	set "PATH=%PATH%;%GRUNT_HOME%"
 )
 
 exit /b 0
